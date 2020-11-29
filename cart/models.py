7447365010a6fb1642eece114b1c31eab242e578
Wyesumu <34547370+Wyesumu@users.noreply.github.com>
2020-11-29 18:44:45 +0700

    Update models.py

--- cart/models.py
+++ cart/models.py
@@ -36,7 +36,7 @@ class Item(models.Model):
     cart = models.ForeignKey(Cart, verbose_name=_('cart'), on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name=_('quantity'))
     shop_id = models.PositiveIntegerField(verbose_name=_('shop id'), default=0)
-    unit_price = models.DecimalField(max_digits=18, decimal_places=2, verbose_name=_('unit price'))
+    unit_price = models.FloatField(verbose_name=_('unit price'))
     # product as generic relation
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
